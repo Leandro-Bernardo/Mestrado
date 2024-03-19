5d5378d1786d4c4b0cbaf0fc5413f775e363fb6b
Leandro-Bernardo <leandro.pbernardo@hotmail.com>
2024-03-18 23:09:35 -0300

    Pequenas alterações e correções em statistical analysis

--- Scripts/statistical_analysis.py
+++ Scripts/statistical_analysis.py
@@ -18,7 +18,7 @@ else:
 EPOCHS = 5
 LR = 0.0001
 BATCH_SIZE = 64
-EVALUATION_BATCH_SIZE = 64
+EVALUATION_BATCH_SIZE = 1
 GRADIENT_CLIPPING_VALUE = 0.5
 MODEL_VERSION = 'model_1' #if len(os.listdir("./models")) == 0 else f'model_{len(os.listdir("./models"))}'
 DATASET_SPLIT = 0.8
@@ -28,7 +28,6 @@ DESCRIPTORS_ROOT = os.path.join(os.path.dirname(__file__), "..", "descriptors")
 EVALUATION_ROOT = os.path.join(os.path.dirname(__file__), "evaluation")
 LAST_CHECKPOINT = sorted(os.listdir(os.path.join(CHECKPOINT_ROOT, MODEL_VERSION)), key = lambda x: int(x.split('_')[-1]))[-1]
 CHECKPOINT_PATH = os.path.join(CHECKPOINT_ROOT, MODEL_VERSION, LAST_CHECKPOINT)
-
 
 print('Using this checkpoint:', CHECKPOINT_PATH)
 
@@ -58,6 +57,7 @@ print(f"X_test, y_test size: {X_test.size()}, {y_test.size()}")
 train_loader = DataLoader(list(zip(X_train, y_train)), batch_size = BATCH_SIZE, shuffle= True)
 eval_loader = DataLoader(list(zip(X_test, y_test)), batch_size = EVALUATION_BATCH_SIZE, shuffle = False)
 
+# clears data from memory
 del X_train
 del y_train
 del X_test
@@ -85,7 +85,7 @@ checkpoint = torch.load(CHECKPOINT_PATH)
 model.load_state_dict(checkpoint['model_state_dict'])
 optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
-
+# utilities functions
 def train_epoch(model, train_loader, optimizer, loss_fn):
     model.train()
     total_loss = 0
@@ -98,7 +98,6 @@ def train_epoch(model, train_loader, optimizer, loss_fn):
         optimizer.step()
         total_loss += loss.item()
     return total_loss / len(train_loader)
-
 
 def evaluate(model, eval_loader, loss_fn):
 
@@ -138,36 +137,36 @@ for actual_epoch in tqdm(range(EPOCHS)):
 
 #evaluation
 print("Evaluation time")
-_, predicted_value, expected_value = evaluate(model,eval_loader, loss_fn)
+partial_loss, predicted_value, expected_value = evaluate(model,eval_loader, loss_fn)
 
 
-#with open(f"./evaluation/predicted_values/{MODEL_VERSION}/{MODEL_VERSION}.txt", "w") as file: # overrides if file exists
-with open(os.path.join(EVALUATION_ROOT, "predicted_values", MODEL_VERSION, f"{MODEL_VERSION}.txt", "w")) as file: # overrides if file exists
+with open(os.path.join(EVALUATION_ROOT, "predicted_values", MODEL_VERSION, f"{MODEL_VERSION}.txt"), "w") as file: # overrides if file exists
     file.write("predicted_value,expected_value\n")
 
-with open(os.path.join(EVALUATION_ROOT, "predicted_values", MODEL_VERSION, f"{MODEL_VERSION}.txt", "a+")) as file:
+with open(os.path.join(EVALUATION_ROOT, "predicted_values", MODEL_VERSION, f"{MODEL_VERSION}.txt"), "a+") as file:
     for i in range(len(predicted_value)):
         file.write(f"{predicted_value[i]},{expected_value[i]}\n")
 
 #histograms
 print("calculating histograms of predictions\n")
-predicted_value_for_samples = {f"sample_{i}" : value for i, value in enumerate(np.reshape(predicted_value,( -1, 112,112)))}
-expected_value_from_samples = {f"sample_{i}" : value for i, value in enumerate(np.reshape(expected_value,( -1, 112,112)))}
-for i in range(int(len(eval_loader)/(112*112))):
+predicted_value_for_samples = {f"sample_{i}" : value for i, value in enumerate(np.reshape(predicted_value,( -1, 94, 94)))}
+expected_value_from_samples = {f"sample_{i}" : value for i, value in enumerate(np.reshape(expected_value,( -1, 94, 94)))}
+
+for i in range(int(len(eval_loader)/(94*94))):
     values = predicted_value_for_samples[f'sample_{i}']
     plt.hist(values, edgecolor ='black')
     plt.title(f"Sample_{i}, Expected Value: {round(expected_value_from_samples[f'sample_{i}'][0][0], 2)}")
     plt.xlabel('Prediction')
     plt.ylabel('Count')
-    plt.savefig(os.path.join(EVALUATION_ROOT, "histogram", MODEL_VERSION, f"sample_{i}.png"))
+    plt.savefig(os.path.join(EVALUATION_ROOT, "histogram", MODEL_VERSION, f"sample_{i + train_split_size + 1}.png"))
     plt.close('all')
 
-# reshapes to the size of the output from the first cnn in vgg11 (112,112) and the total of images (len(eval_loader)/(112*112) = 695)
+# reshapes to the size of the output from the first cnn in vgg11 (112 - 18, 112 - 18) and the total of images (len(eval_loader)/(112*112) = 695)
 print("reshaping images to match cnn1 output\n")
-partial_loss_from_cnn1_output = np.reshape(partial_loss, -1, 112,112)
+partial_loss_from_cnn1_output = np.reshape(partial_loss, -1, 94, 94)
 
 for i, image in enumerate(partial_loss_from_cnn1_output):
-    plt.imsave(os.path.join(EVALUATION_ROOT, "error_from_image","from_cnn1_output", MODEL_VERSION, f"sample_{i}.png"), image)
+    plt.imsave(os.path.join(EVALUATION_ROOT, "error_from_image","from_cnn1_output", MODEL_VERSION, f"sample_{i + train_split_size + 1}.png"), image)
 
 
 # resize to the original input size (224,224)
@@ -178,14 +177,14 @@ for image in partial_loss_from_cnn1_output:
     partial_loss_from_original_image.append(resized_image)
 
 for i, error_map in enumerate(partial_loss_from_original_image):
-    original_image = cv2.cvtColor(cv2.imread(f"./images/sample_{i}.png"), cv2.COLOR_BGR2RGB)
+    original_image = cv2.cvtColor(cv2.imread(f"./images/sample_{i + train_split_size + 1}.png"), cv2.COLOR_BGR2RGB)
 
     #plots error map and original image sidewise
     fig,ax = plt.subplots(nrows=1,ncols=2)
-    fig.suptitle(f"Sample_{i}: error map  X  original image")
+    fig.suptitle(f"Sample_{i + train_split_size + 1}: error map  X  original image")
     ax[0].imshow(error_map)
     ax[1].imshow(original_image)
-    plt.savefig(os.path.join(EVALUATION_ROOT, "error_from_image", "from_original_image", MODEL_VERSION, f"sample_{i}.png"))
+    plt.savefig(os.path.join(EVALUATION_ROOT, "error_from_image", "from_original_image", MODEL_VERSION, f"sample_{i + train_split_size + 1}.png"))
     plt.close('all')
 
     #plt.imsave(f"./evaluation/error_from_image/from_original_image/{MODEL_VERSION}/sample_{i}.png", image)
