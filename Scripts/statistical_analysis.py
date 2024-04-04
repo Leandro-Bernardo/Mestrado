54764d8dbdf9891ac01dae2cc51658720024558c
Leandro-Bernardo <leandro.pbernardo@hotmail.com>
2024-04-04 14:09:02 -0300

    adicionado calculo de estatisticas basicas e pequenas alteracoes

--- Scripts/statistical_analysis.py
+++ Scripts/statistical_analysis.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+import scipy.stats
 
 from tqdm import tqdm
 #from sklearn.model_selection import train_test_split
@@ -57,6 +58,9 @@ print(f"X_test, y_test size: {X_test.size()}, {y_test.size()}")
 train_loader = DataLoader(list(zip(X_train, y_train)), batch_size = BATCH_SIZE, shuffle= True)
 eval_loader = DataLoader(list(zip(X_test, y_test)), batch_size = EVALUATION_BATCH_SIZE, shuffle = False)
 
+# saves values for graph scale
+min_value, max_value = torch.min(y_test), torch.max(y_test)
+
 # clears data from memory
 del X_train
 del y_train
@@ -65,7 +69,7 @@ del y_test
 
 torch.cuda.empty_cache()
 
-#model definition
+# model definition
 model = torch.nn.Sequential(
     torch.nn.Linear(in_features=448, out_features=256),
     torch.nn.ReLU(),
@@ -85,7 +89,7 @@ checkpoint = torch.load(CHECKPOINT_PATH)
 model.load_state_dict(checkpoint['model_state_dict'])
 optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
-# utilities functions
+# utilities functions and classes
 def train_epoch(model, train_loader, optimizer, loss_fn):
     model.train()
     total_loss = 0
@@ -128,63 +132,86 @@ def evaluate(model, eval_loader, loss_fn):
     return partial_loss, predicted_value, expected_value # ,accuracy
 
 
-#training
-print("Training time")
-for actual_epoch in tqdm(range(EPOCHS)):
-    train_loss = train_epoch(model, train_loader, optimizer, loss_fn)
+class Statistics():
 
-    print(f"Epoch {actual_epoch + 1}, Loss: {train_loss}")
-
-#evaluation
-print("Evaluation time")
-partial_loss, predicted_value, expected_value = evaluate(model,eval_loader, loss_fn)
-
-
-with open(os.path.join(EVALUATION_ROOT, "predicted_values", MODEL_VERSION, f"{MODEL_VERSION}.txt"), "w") as file: # overrides if file exists
-    file.write("predicted_value,expected_value\n")
-
-with open(os.path.join(EVALUATION_ROOT, "predicted_values", MODEL_VERSION, f"{MODEL_VERSION}.txt"), "a+") as file:
-    for i in range(len(predicted_value)):
-        file.write(f"{predicted_value[i]},{expected_value[i]}\n")
-
-#histograms
-print("calculating histograms of predictions\n")
-predicted_value_for_samples = {f"sample_{i}" : value for i, value in enumerate(np.reshape(predicted_value,( -1, 94, 94)))}
-expected_value_from_samples = {f"sample_{i}" : value for i, value in enumerate(np.reshape(expected_value,( -1, 94, 94)))}
-
-for i in range(int(len(eval_loader)/(94*94))):
-    values = predicted_value_for_samples[f'sample_{i}']
-    plt.hist(values, edgecolor ='black')
-    plt.title(f"Sample_{i}, Expected Value: {round(expected_value_from_samples[f'sample_{i}'][0][0], 2)}")
-    plt.xlabel('Prediction')
-    plt.ylabel('Count')
-    plt.savefig(os.path.join(EVALUATION_ROOT, "histogram", MODEL_VERSION, f"sample_{i + train_split_size + 1}.png"))
-    plt.close('all')
-
-# reshapes to the size of the output from the first cnn in vgg11 (112 - 18, 112 - 18) and the total of images (len(eval_loader)/(112*112) = 695)
-print("reshaping images to match cnn1 output\n")
-partial_loss_from_cnn1_output = np.reshape(partial_loss, -1, 94, 94)
-
-for i, image in enumerate(partial_loss_from_cnn1_output):
-    plt.imsave(os.path.join(EVALUATION_ROOT, "error_from_image","from_cnn1_output", MODEL_VERSION, f"sample_{i + train_split_size + 1}.png"), image)
+    def __init__(self, sample_predictions_vector):
+        self.mean = scipy.stats.mean(sample_predictions_vector)
+        self.median = scipy.stats.median(sample_predictions_vector)
+        self.mode = scipy.stats.mode(sample_predictions_vector)
+        self.variance = scipy.stats.variation(sample_predictions_vector)
+        self.std = scipy.stats.tstd(sample_predictions_vector) 
+        self.min_value = min(sample_predictions_vector)
+        self.max_value = max(sample_predictions_vector)
 
 
-# resize to the original input size (224,224)
-print("resizing images to original size\n")
-partial_loss_from_original_image = []#np.resize(partial_loss_from_cnn1_output, (224,224))
-for image in partial_loss_from_cnn1_output:
-    resized_image = cv2.resize(image, (224, 224), interpolation = cv2.INTER_NEAREST)
-    partial_loss_from_original_image.append(resized_image)
+def main():
+    #training
+    print("Training time")
+    for actual_epoch in tqdm(range(EPOCHS)):
+        train_loss = train_epoch(model, train_loader, optimizer, loss_fn)
 
-for i, error_map in enumerate(partial_loss_from_original_image):
-    original_image = cv2.cvtColor(cv2.imread(f"./images/sample_{i + train_split_size + 1}.png"), cv2.COLOR_BGR2RGB)
+        print(f"Epoch {actual_epoch + 1}, Loss: {train_loss}")
 
-    #plots error map and original image sidewise
-    fig,ax = plt.subplots(nrows=1,ncols=2)
-    fig.suptitle(f"Sample_{i + train_split_size + 1}: error map  X  original image")
-    ax[0].imshow(error_map)
-    ax[1].imshow(original_image)
-    plt.savefig(os.path.join(EVALUATION_ROOT, "error_from_image", "from_original_image", MODEL_VERSION, f"sample_{i + train_split_size + 1}.png"))
-    plt.close('all')
+    #evaluation
+    print("Evaluation time")
+    partial_loss, predicted_value, expected_value = evaluate(model,eval_loader, loss_fn)
 
-    #plt.imsave(f"./evaluation/error_from_image/from_original_image/{MODEL_VERSION}/sample_{i}.png", image)
+
+    with open(os.path.join(EVALUATION_ROOT, "predicted_values", MODEL_VERSION, f"{MODEL_VERSION}.txt"), "w") as file: # overrides if file exists
+        file.write("predicted_value,expected_value\n")
+
+    with open(os.path.join(EVALUATION_ROOT, "predicted_values", MODEL_VERSION, f"{MODEL_VERSION}.txt"), "a+") as file:
+        for i in range(len(predicted_value)):
+            file.write(f"{predicted_value[i]},{expected_value[i]}\n")
+
+    #histograms
+    print("calculating histograms of predictions\n")
+    predicted_value_for_samples = {f"sample_{i}" : value for i, value in enumerate(np.reshape(predicted_value,( -1, 94, 94)))}
+    expected_value_from_samples = {f"sample_{i}" : value for i, value in enumerate(np.reshape(expected_value,( -1, 94, 94)))}
+
+    for i in range(int(len(eval_loader)/(94*94))):
+        values = predicted_value_for_samples[f'sample_{i}']
+        stats = Statistics(predicted_value_for_samples[f'sample_{i}'])
+        plt.hist(values, edgecolor ='black')
+        plt.title(f"Sample_{i}, Expected Value: {round(expected_value_from_samples[f'sample_{i}'][0][0], 2)}")
+        plt.xlabel('Prediction')
+        plt.ylabel('Count')
+        plt.xlim((min_value, max_value))
+        plt.text(x = max_value + 0.5,  y = 0,
+                 s = f" mean: {stats.mean}\n median: {stats.median}\n mode: {stats.mode}\n var: {stats.variance:.2}\n std: {stats.std:.2}\n min: {stats.min_value}\n max: {stats.max_value}")
+        plt.savefig(os.path.join(EVALUATION_ROOT, "histogram", MODEL_VERSION, f"sample_{i + train_split_size + 1}.png"))
+        plt.close('all')
+
+    # reshapes to the size of the output from the first cnn in vgg11 (112 - 18, 112 - 18) and the total of images (len(eval_loader)/(112*112) = 695)
+    print("reshaping images to match cnn1 output\n")
+    partial_loss_from_cnn1_output = np.reshape(partial_loss, -1, 94, 94)
+
+    for i, image in enumerate(partial_loss_from_cnn1_output):
+        plt.imsave(os.path.join(EVALUATION_ROOT, "error_from_image","from_cnn1_output", MODEL_VERSION, f"sample_{i + train_split_size + 1}.png"), image)
+
+
+    # resize to the original input size (224,224)
+    print("resizing images to original size\n")
+    partial_loss_from_original_image = []#np.resize(partial_loss_from_cnn1_output, (224,224))
+    for image in partial_loss_from_cnn1_output:
+        resized_image = cv2.resize(image, (224, 224), interpolation = cv2.INTER_NEAREST)
+        partial_loss_from_original_image.append(resized_image)
+
+    for i, error_map in enumerate(partial_loss_from_original_image):
+        original_image = cv2.cvtColor(cv2.imread(f"./images/sample_{i + train_split_size + 1}.png"), cv2.COLOR_BGR2RGB)
+
+        #plots error map and original image sidewise
+        fig,ax = plt.subplots(nrows=1,ncols=2)
+        fig.suptitle(f"Sample_{i + train_split_size + 1}: error map  X  original image")
+        ax[0].imshow(error_map)
+        ax[1].imshow(original_image)
+        plt.savefig(os.path.join(EVALUATION_ROOT, "error_from_image", "from_original_image", MODEL_VERSION, f"sample_{i + train_split_size + 1}.png"))
+        plt.close('all')
+
+        #plt.imsave(f"./evaluation/error_from_image/from_original_image/{MODEL_VERSION}/sample_{i}.png", image)
+
+    write_pdf_statistics()
+
+
+if __name__ == "__main__":
+    main()
