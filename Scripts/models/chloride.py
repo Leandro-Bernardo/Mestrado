cb4d553be35d71521a2cace2956e2e9b279aade7
Leandro Bernardo <leandro.pbernardo@outlook.com>
2024-08-06 14:31:30 -0300

    Adicionado modelo Zero Dawn
    A profundidade da hidden layer (e quantidades de neuronios) agora são hiperparametros do sweep

--- Scripts/models/chloride.py
+++ Scripts/models/chloride.py
@@ -4,12 +4,12 @@ import torch.nn as nn
 from typing import Optional, Dict
 
 class Model_1(torch.nn.Module):
-    def __init__(self, in_channels: int = 1472, device: str = "cuda"):
+    def __init__(self, descriptor_depth: int = 1472, sweep_config: Optional[Dict] = None, device: str = "cuda", **kwargs):
         super().__init__()
 
         device: str = "cuda"
         self.in_layer = nn.Sequential(
-            torch.nn.Linear(in_features = in_channels, out_features=1024),
+            torch.nn.Linear(in_features = descriptor_depth, out_features=1024),
            # nn.BatchNorm1d(1024),
             torch.nn.ReLU())
         self.l1 = nn.Sequential(
@@ -48,11 +48,11 @@ class Model_1(torch.nn.Module):
 
 
 class Model_2(torch.nn.Module):
-    def __init__(self, in_channels: int  = 3904, device: str = "cuda"):
+    def __init__(self, descriptor_depth: int = 3904, sweep_config: Optional[Dict] = None, device: str = "cuda", **kwargs):
         super().__init__()
 
         self.in_layer = nn.Sequential(
-            torch.nn.Linear(in_features= in_channels, out_features=4096),
+            torch.nn.Linear(in_features= descriptor_depth, out_features=4096),
             nn.BatchNorm1d(4096),
             torch.nn.ReLU())
         self.l1 = nn.Sequential(
@@ -101,7 +101,7 @@ class Model_2(torch.nn.Module):
 
 
 class Model_3(torch.nn.Module):
-    def __init__(self, in_channels: int = 1856, device: str = "cuda", sweep_config: Optional[Dict] = None):
+    def __init__(self, descriptor_depth: int = 1856, sweep_config: Optional[Dict] = None, device: str = "cuda", **kwargs):
         super().__init__()
 
         self.dropoutL1 = nn.Dropout(p = sweep_config["dropout_upper_layers"])  if sweep_config is not None else nn.Dropout(p=0.5)
@@ -111,28 +111,28 @@ class Model_3(torch.nn.Module):
 
         device: str = "cuda"
         self.in_layer = nn.Sequential(
-            torch.nn.Linear(in_features = in_channels, out_features=1024),
-           # nn.BatchNorm1d(1024),
+            torch.nn.Linear(in_features = descriptor_depth, out_features=1024),
+            # nn.BatchNorm1d(1024),
             torch.nn.ReLU())
         self.l1 = nn.Sequential(
             torch.nn.Linear(in_features=1024, out_features=512),
-           # nn.BatchNorm1d(512),
+            # nn.BatchNorm1d(512),
             torch.nn.ReLU())
         self.l2 = nn.Sequential(
             torch.nn.Linear(in_features=512, out_features=256),
-           # nn.BatchNorm1d(256),
+            # nn.BatchNorm1d(256),
             torch.nn.ReLU())
         self.l3 = nn.Sequential(
             torch.nn.Linear(in_features=256, out_features=128),
-           # nn.BatchNorm1d(128),
+            # nn.BatchNorm1d(128),
             torch.nn.ReLU())
         self.l4 = nn.Sequential(
             torch.nn.Linear(in_features=128, out_features=64),
-          # nn.BatchNorm1d(64),
+            # nn.BatchNorm1d(64),
             torch.nn.ReLU())
         self.l5 = nn.Sequential(
             torch.nn.Linear(in_features=64, out_features=32),
-           # nn.BatchNorm1d(32),
+            # nn.BatchNorm1d(32),
             torch.nn.ReLU())
         self.output_layer = nn.Sequential(
             torch.nn.Linear(in_features=32, out_features=1))
@@ -154,7 +154,7 @@ class Model_3(torch.nn.Module):
 
 
 class Model_4(torch.nn.Module):
-    def __init__(self, in_channels: int = 1856, device: str = "cuda", sweep_config: Optional[Dict] = None):
+    def __init__(self, descriptor_depth: int = 1856, sweep_config: Optional[Dict] = None, device: str = "cuda", **kwargs):
         super().__init__()
 
         self.dropoutL1 = nn.Dropout(p = sweep_config["dropout_upper_layers"])  if sweep_config is not None else nn.Dropout(p=0.5)
@@ -197,3 +197,89 @@ class Model_4(torch.nn.Module):
         x = self.output_layer(x)
 
         return x
+
+
+class Zero_Dawn(torch.nn.Module):
+    def __init__(self, descriptor_depth: int, sweep_config: Dict, **kwargs) -> None:
+        super().__init__()
+
+        self.in_channels = descriptor_depth
+        self.sweep_config = sweep_config
+
+        self.dropoutUpperLayers = nn.Dropout(p=sweep_config["dropout_upper_layers"]) if sweep_config is not None else nn.Dropout(p=0.5)
+        self.dropoutMiddleLayers = nn.Dropout(p=sweep_config["dropout_middle_layers"]) if sweep_config is not None else nn.Dropout(p=0.4)
+        self.dropoutLowerLayers = nn.Dropout(p=sweep_config["dropout_lower_layers"]) if sweep_config is not None else nn.Dropout(p=0.2)
+
+        self.num_hidden_layers = self.sweep_config["num_hidden_layers"]
+
+        self.layers_neuron = {f"layer_{i}": self.sweep_config[f"layer_{i}"] for i in range(self.num_hidden_layers, 0, -1)} # neuron is the output param
+        self.layers_neuron["output_layer"] = self.sweep_config["output_layer"]
+
+        # assures first layer's neuron (output) is lower than the descriptor depth
+        self.layers_neuron[f"input_layer"] = self.largest_power_of_2_less_than_or_equal_to(descriptor_depth)
+        # while self.layers_neuron[f"layer_{self.num_hidden_layers}"] > descriptor_depth:
+        #     self.layers_neuron[f"layer_{self.num_hidden_layers}"] = self.power_2()
+
+        # assures lower layers have same or fewer neurons (output) than higher layers
+        for i in range(1, self.num_hidden_layers):
+            if self.layers_neuron[f"layer_{i}"] > self.layers_neuron[f"layer_{i+1}"]:
+                self.layers_neuron[f"layer_{i+1}"] = self.layers_neuron[f"layer_{i}"]
+
+        # encapsulates inputs and outputs values in one object
+        self.layersIO = {
+                        f"layer_{i}": {
+                            "in": self.layers_neuron[f"layer_{i+1}"] if i < self.num_hidden_layers else self.layers_neuron[f"input_layer"],
+                            "out": self.layers_neuron[f"layer_{i}"],
+                            "dropout": self.dropout(i)
+                        } for i in range(self.num_hidden_layers, 0, -1)}
+        self.layersIO["input_layer"] = {"in": descriptor_depth, "out": self.layers_neuron["input_layer"]}
+        self.layersIO["output_layer"] = {"in":self.layersIO["layer_1"]["out"], "out": self.layers_neuron["output_layer"]}
+
+        # builds the neural network ...
+        self.sequential_layers = nn.ModuleDict()
+        # ... input layer
+        self.sequential_layers["input_layer"] = nn.Sequential(
+                                                             torch.nn.Linear(in_features=self.layersIO["input_layer"]["in"], out_features=self.layersIO["input_layer"]["out"]),
+                                                             torch.nn.ReLU(),
+                                                             nn.Dropout(p=self.layersIO[f"layer_{self.num_hidden_layers}"]["dropout"]))
+        # ... hidden layers
+        for i in range(self.num_hidden_layers, 0, -1):
+            self.sequential_layers[f"layer_{i}"] = nn.Sequential(
+                                                                torch.nn.Linear(in_features=self.layersIO[f"layer_{i}"]["in"], out_features=self.layersIO[f"layer_{i}"]["out"]),
+                                                                torch.nn.ReLU(),
+                                                                nn.Dropout(p=self.layersIO[f"layer_{i}"]["dropout"]) if i > 1 else nn.Dropout(p=0))
+        # ... output layer
+        self.sequential_layers["output_layer"] = nn.Sequential(
+                                                              torch.nn.Linear(in_features=self.layersIO["output_layer"]["in"], out_features=self.layersIO["output_layer"]["out"]))
+
+    def forward(self, x: torch.Tensor):
+        # input layer
+        x = self.sequential_layers["input_layer"](x)
+        # hidden layers
+        for i in range(self.num_hidden_layers, 0, -1):
+            x = self.sequential_layers[f"layer_{i}"](x)
+        # output layer
+        x = self.sequential_layers["output_layer"](x)
+        return x
+
+    # util generator function
+    # def power_2(self):
+    #     for i in range(self.num_hidden_layers, 0, -1):
+    #         yield 2**i
+
+    #Return the largest power of 2 less than or equal to n (to be the input of the input_layer)
+    def largest_power_of_2_less_than_or_equal_to(self, descriptor_depth):
+        return 2**(descriptor_depth.bit_length() - 1)
+
+    # dropout values for ...
+    def dropout(self, layer_num):
+        division = self.num_hidden_layers // 3
+        # ... upper layers
+        if layer_num >= 2 * division:
+            return self.sweep_config["dropout_upper_layers"]
+        # ... lower layers
+        elif layer_num <= division:
+            return self.sweep_config["dropout_lower_layers"]
+        # ... middle layers
+        else:
+            return self.sweep_config["dropout_middle_layers"]
