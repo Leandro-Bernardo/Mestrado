9ccca590edad522c612c41dbef1141832b4c0638
Leandro Bernardo <leandro.pbernardo@outlook.com>
2024-09-18 08:26:07 -0300

    Alterado scripts/lightning.py
    Adicionado um custom dataset (torch) para gerenciar o carregamento de multiplos datasets

--- Scripts/models/lightning.py
+++ Scripts/models/lightning.py
@@ -4,6 +4,9 @@ import json
 import pytorch_lightning as pl
 import torch.nn as nn
 import numpy as np
+import pandas as pd
+
+from random import randint
 
 from pytorch_lightning import LightningDataModule, LightningModule, Trainer
 from torch import FloatTensor, UntypedStorage
@@ -14,50 +17,86 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torchmetrics import Accuracy, F1Score, JaccardIndex, MeanAbsoluteError, MeanAbsolutePercentageError, MeanSquaredError, MetricCollection, Precision, Recall, SymmetricMeanAbsolutePercentageError, WeightedMeanAbsolutePercentageError
 from typing import Any, List, Tuple, TypeVar, Optional, Dict
 
+class CustomDataset(Dataset):
+    def __init__(self, *, dataset_root: str, stage: str, dataset_idx: int = 0):
+        self.dataset_root = dataset_root
+        self.stage = stage
+        self.dataset_idx = dataset_idx
+        self.mapping = pd.read_csv(os.path.join(self.dataset_root, f"{self.stage}_mapping.csv")).set_index("descriptor_index")
+        # holds one dataset in memory
+        print(f"Initializing {stage} dataset")
+        self.current_dataset = torch.load(os.path.join(self.dataset_root, f"descriptors_{self.stage}_dataset_{self.dataset_idx}.pt"))
+        self.current_dataset_anotation = torch.load(os.path.join(self.dataset_root, f"descriptors_anotation_{self.stage}_dataset_{self.dataset_idx}.pt"))
 
+    def __len__(self):
+        with open(os.path.join(self.dataset_root, f'metadata_{self.stage}.json'), "r") as file:
+            metadata = json.load(file)
+            total_samples = metadata['total_samples']
+            cnn1_output_size = metadata['image_size']
+        return int(total_samples*cnn1_output_size)
+
+    def __getitem__(self, idx):
+        sample_dataset_num = self.mapping.loc[idx, "dataset_num"]  #the dataset (splited) that contains the required sample
+        sample_dataset_idx = self.mapping.loc[idx, "dataset_descriptor_index"]  #the position of the current sample (idx) on the specific dataset
+        if sample_dataset_num == self.dataset_idx:
+            # gets the descriptor (X)
+            descriptor_sample = self.current_dataset[sample_dataset_idx]
+            # gets the anotation (Y)
+            expected_value_sample = self.current_dataset_anotation[sample_dataset_idx]
+
+        elif sample_dataset_num != self.dataset_idx:
+            print(f"changed dataset from {self.dataset_idx} to {sample_dataset_num} ")
+            # clears memory
+            del self.current_dataset
+            del self.current_dataset_anotation
+            torch.cuda.empty_cache()
+            # updates tracked index
+            self.dataset_idx = sample_dataset_num
+            # updates dataset
+            self.current_dataset = torch.load(os.path.join(self.dataset_root, f"descriptors_{self.stage}_dataset_{self.dataset_idx}.pt"))
+            self.current_dataset_anotation = torch.load(os.path.join(self.dataset_root, f"descriptors_anotation_{self.stage}_dataset_{self.dataset_idx}.pt"))
+            # gets the descriptor (X)
+            descriptor_sample = self.current_dataset[sample_dataset_idx]
+            # gets the anotation (Y)
+            expected_value_sample = self.current_dataset_anotation[sample_dataset_idx]
+
+        return (descriptor_sample, expected_value_sample)
 
 class DataModule(LightningDataModule):
-    def __init__(self, *, descriptor_root: str, stage: str, batch_size: int, num_datasets: int, num_workers: int):
+    def __init__(self, *, descriptor_root: str, stage: str, batch_size: int, num_datasets_training: int = 1, num_datasets_validation: int = 1, num_datasets_test: int = 1, num_workers: int):
         super().__init__()
         self.descriptor_root = descriptor_root
         self.batch_size = batch_size
-        self.num_datasets = num_datasets
         self.num_workers = num_workers
+        #TODO validar se use_persistent_workers() funciona da forma correta
+        self.num_datasets_training = num_datasets_training
+        self.num_datasets_validation = num_datasets_validation
+        self.num_datasets_test = num_datasets_test
         self.save_hyperparameters() # saves hyperparameters in checkpoint file
-
-    def _load_dataset(self, descriptor_root: str, stage: str, dataset_index):
-        with open(os.path.join(descriptor_root, f'metadata_{stage}_dataset{dataset_index}.json'), "r") as file:
-            metadata = json.load(file)
-        total_samples = metadata['total_samples']
-        image_shape = metadata['image_shape']
-        image_size = metadata['image_size']
-        descriptor_depth = metadata['descriptor_depth']
-        nbytes_float32 = torch.finfo(torch.float32).bits//8
-        #NOTE: at the moment, descriptors are saved in the format (num samples, image_size, descriptors_depth), but they are read in format (num samples * image_size,descriptors_depth).
-        #      expected_value is saved in format (num samples, image_size), and read in format (num samples * image_size)
-        #NOTE 2: changed (now both are written and readed in same format)
-        descriptors = FloatTensor(UntypedStorage.from_file(os.path.join(self.descriptor_root, f"descriptors_{stage}_dataset{dataset_index}.bin"), shared = False, nbytes= (total_samples * image_size * descriptor_depth) * nbytes_float32)).view(total_samples * image_size, descriptor_depth)
-        expected_value = FloatTensor(UntypedStorage.from_file(os.path.join(self.descriptor_root, f"descriptors_anotation_{stage}_dataset{dataset_index}.bin"), shared = False, nbytes= (total_samples * image_size) * nbytes_float32)).view(total_samples * image_size)
-
-        return TensorDataset(descriptors, expected_value)
 
     def setup(self, stage: str) -> None:  # all the same because the dataset is splited and saved in disk
         if stage == "fit":
-            self.train_subset = self._load_dataset(self.descriptor_root, "train")
-            self.val_subset = self._load_dataset(self.descriptor_root, "val")
+            self.train_subset = CustomDataset(dataset_root=self.descriptor_root, stage="train")
+            self.val_subset = CustomDataset(dataset_root=self.descriptor_root, stage="val")
         elif stage == "validate":
-            self.val_subset = self._load_dataset(self.descriptor_root, "val")
+            self.val_subset = CustomDataset(dataset_root=self.descriptor_root, stage="val")
         elif stage == "test":
-            self.test_subset = self._load_dataset(self.descriptor_root, "test")
+            self.test_subset = CustomDataset(dataset_root=self.descriptor_root, stage="test")
 
-    def train_dataloader(self):
-        return DataLoader(self.train_subset, batch_size=self.batch_size, num_workers=self.num_workers, persistent_workers=True, shuffle= True, drop_last=True)
+    def train_dataloader(self): #NOTE disabled persistent_workers so datasets wont be saved in memory
+        return DataLoader(self.train_subset, batch_size=self.batch_size, num_workers=self.num_workers, persistent_workers=False, shuffle=True, drop_last=True, pin_memory=True)
 
     def val_dataloader(self):
-        return DataLoader(self.val_subset, batch_size=self.batch_size, num_workers=self.num_workers, persistent_workers=True, shuffle= False, drop_last=True)
+        return DataLoader(self.val_subset, batch_size=self.batch_size, num_workers=self.num_workers, persistent_workers=False, shuffle=False, drop_last=True, pin_memory=True)
 
     def test_dataloader(self):
-        return DataLoader(self.test_subset,  batch_size=1, num_workers=self.num_workers, persistent_workers=True, shuffle= False, drop_last=True)
+        return DataLoader(self.test_subset,  batch_size=1, num_workers=self.num_workers, persistent_workers=False, shuffle=False, drop_last=True, pin_memory=True)
+
+    def use_persistent_workers(self):
+        if self.num_datasets_training > 1 or self.num_datasets_validation > 1 or self.num_datasets_test > 1:
+            return False
+        elif self.num_datasets_training == 1 and self.num_datasets_validation == 1 and self.num_datasets_test == 1:
+            return True
 
 class BaseModel(LightningModule):
     def __init__(self, *, dataset: DataLoader, model: torch.nn.Module, batch_size: int, loss_function: torch.nn.Module, learning_rate: float, learning_rate_patience: int = None, descriptor_depth: int, sweep_config: Dict, **kwargs: Any):
@@ -115,10 +154,6 @@ class BaseModel(LightningModule):
     def test_step(self, batch: List[torch.tensor]):#, batch_idx: int):
         return self._any_step(batch, "Test")
 
-    def on_train_batch_end(self, *args, **kwargs):
-        pass
-
-
     def _any_epoch_end(self, stage: str):
         metrics: MetricCollection = self.metrics[stage]  # type: ignore
         self.log_dict({f'{metric_name}/{stage}/Epoch': value for metric_name, value in metrics.compute().items()}, on_step=False, on_epoch=True) # logs metrics on epoch end
@@ -137,6 +172,9 @@ class BaseModel(LightningModule):
         self._any_epoch_end("Test")
 
 
+    def on_batch_end():
+
+        pass
     def predict_step(self, batch, batch_idx):
         # change model to evaluation mode
         #model.eval()
