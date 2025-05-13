d47ed702cefb3e5b6554d3e74f67ec852cb3a69f
Leandro Bernardo <leandro.pbernardo@outlook.com>
2025-05-13 13:27:24 -0300

    adicionado script para fazer amostragem dos resultados

---
+++ Scripts/utils/random_choices.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from random import choices
+import os
+
+ANALYTE = 'pH'
+num_of_samples = 40
+
+path = os.path.join(os.path.dirname(__file__), "..", "evaluation", f"{ANALYTE}")
+excel_path = os.path.join(path, f"{ANALYTE}_planilha_dissertacao.xlsx")
+
+excel = pd.read_excel(excel_path).set_index('Sample')
+excel.index.name = 'Sample'
+
+chosen_samples = choices(excel.index, k = num_of_samples)
+
+chosen_samples_df = pd.DataFrame()
+for sample in chosen_samples:
+    chosen_samples_df[sample] = excel.loc[sample,:]
+
+chosen_samples_df = chosen_samples_df.transpose()
+chosen_samples_df.to_excel(os.path.join(path,'chosen_samples.xlsx'))
