653841e1d834461e001416f88945d6907011c8b6
Leandro Bernardo <leandro.pbernardo@outlook.com>
2025-05-14 07:44:08 -0300

    Pequenas alterações

--- Scripts/utils/concatenate_inferences.py
+++ Scripts/utils/concatenate_inferences.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import os
 
-path_pmf_model_inference = os.path.join(os.path.dirname(__file__), "..",  "evaluation", "Chloride", "Chloride_Reinjecao_model.xlsx")
-path_descriptor_model_inference = os.path.join(os.path.dirname(__file__), "..", "evaluation", "Chloride", "with_blank", "resnet50(3_blocks)", "best_model_3blocks_resnet50_img_size_448", "test", "statistics", "resnet50(3_blocks)(image_size_448).xlsx")
+ANALYTE = 'pH'
+IMG_TO_EVALUATE = 'val'
+
+path_pmf_model_inference = os.path.join(os.path.dirname(__file__), "..",  "evaluation", f"{ANALYTE}", f"{ANALYTE}_reinjecao_{IMG_TO_EVALUATE}_samples.xlsx")
+path_descriptor_model_inference = os.path.join(os.path.dirname(__file__), "..", "evaluation", f"{ANALYTE}", f"{ANALYTE}_{IMG_TO_EVALUATE}_samples.xlsx")
 
 pmf_model_inference = pd.read_excel(path_pmf_model_inference).set_index("Imagem da Amostra")
-descriptor_model_inference = pd.read_excel(path_descriptor_model_inference).set_index('Sample')
+descriptor_model_inference = pd.read_excel(path_descriptor_model_inference).set_index('Unnamed: 0') #.set_index('Sample')
+descriptor_model_inference.index.name = 'Sample'
 
 for sample in pmf_model_inference.index:
-    descriptor_model_inference.loc[sample, "new_estimated"] = pmf_model_inference.loc[sample, "Estimativa Offline"]
+    descriptor_model_inference.loc[sample, "estimated"] = pmf_model_inference.loc[sample, "Estimativa Offline"]
 
 descriptor_model_inference.to_excel(path_descriptor_model_inference)
 print("Done!")
--- Scripts/utils/random_choices.py
+++ Scripts/utils/random_choices.py
@@ -2,14 +2,17 @@ import pandas as pd
 from random import choices
 import os
 
-ANALYTE = 'pH'
+ANALYTE = 'Chloride'
 num_of_samples = 40
 
 path = os.path.join(os.path.dirname(__file__), "..", "evaluation", f"{ANALYTE}")
 excel_path = os.path.join(path, f"{ANALYTE}_planilha_dissertacao.xlsx")
 
-excel = pd.read_excel(excel_path).set_index('Sample')
-excel.index.name = 'Sample'
+try:
+    excel = pd.read_excel(excel_path).set_index('Sample')
+    excel.index.name = 'Sample'
+except:
+    excel = pd.read_excel(excel_path)
 
 chosen_samples = choices(excel.index, k = num_of_samples)
 
