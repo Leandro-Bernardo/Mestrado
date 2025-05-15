562c7e9e26e1750acaa3d19265572bb7d1babb4a
Leandro Bernardo <leandro.pbernardo@outlook.com>
2025-05-15 18:17:58 -0300

    Pequenas alteração no script concatenate_inferences.py

--- Scripts/utils/concatenate_inferences.py
+++ Scripts/utils/concatenate_inferences.py
@@ -1,15 +1,18 @@
 import pandas as pd
 import os
 
-ANALYTE = 'pH'
-IMG_TO_EVALUATE = 'val'
+ANALYTE = 'Chloride'
+IMG_TO_EVALUATE = 'test'
 
 path_pmf_model_inference = os.path.join(os.path.dirname(__file__), "..",  "evaluation", f"{ANALYTE}", f"{ANALYTE}_reinjecao_{IMG_TO_EVALUATE}_samples.xlsx")
 path_descriptor_model_inference = os.path.join(os.path.dirname(__file__), "..", "evaluation", f"{ANALYTE}", f"{ANALYTE}_{IMG_TO_EVALUATE}_samples.xlsx")
 
 pmf_model_inference = pd.read_excel(path_pmf_model_inference).set_index("Imagem da Amostra")
-descriptor_model_inference = pd.read_excel(path_descriptor_model_inference).set_index('Unnamed: 0') #.set_index('Sample')
-descriptor_model_inference.index.name = 'Sample'
+try:
+    descriptor_model_inference = pd.read_excel(path_descriptor_model_inference).set_index('Unnamed: 0') #.set_index('Sample')
+    descriptor_model_inference.index.name = 'Sample'
+except:
+    descriptor_model_inference = pd.read_excel(path_descriptor_model_inference).set_index('Sample')
 
 for sample in pmf_model_inference.index:
     descriptor_model_inference.loc[sample, "estimated"] = pmf_model_inference.loc[sample, "Estimativa Offline"]
